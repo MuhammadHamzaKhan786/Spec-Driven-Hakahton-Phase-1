83adcabf77ee966b9eaddedf963b62aee0922d43
ishaqbusiness5666-sudo <ishaqbusiness5666@gmail.com>
2025-12-07 22:08:32 +0500

    Deployment Url Fix

--- robotics-book/docusaurus.config.ts
+++ robotics-book/docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://robotics-textbook.example.com',
+  url: 'https://spec-driven-hakahton-phase-1-mp9g.vercel.app/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
